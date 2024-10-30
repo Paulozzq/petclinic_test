764fc53230f3a893a11719ff683e61c1966c4458
cha1ton <chalton.mercado@tecsup.edu.pe>
2024-10-30 13:40:45 -0500

    Repository

---
+++ src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,6 @@
+package com.tecsup.petclinic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+}
